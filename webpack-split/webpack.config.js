79dfaf625252bbda827265bd9eedefa3545b54ee
kitebeen <ypyp66@naver.com>
2022-01-08 17:29:23 +0900

    웹팩으로 지식을 쌓아보자

--- webpack-split/webpack.config.js
+++ webpack-split/webpack.config.js
@@ -1,38 +1,21 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    page1: "./src/index1.js",
+    page3: "./src/index3.js",
   },
   output: {
     filename: "[name].js",
+    chunkFilename: "[id].chunk.js",
     path: path.resolve(__dirname, "dist"),
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-      //minSize: 10, //bytes
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: 1,
-          name: "vendors",
-        },
-        // defaultVendors: {
-        //   minChunks: 1,
-        //   priority: 1,
-        //   name: "default",
-        // },
-        reactBundle: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: "react.bundle",
-          priority: 2,
-          minSize: 100,
-        },
-      },
-    },
-  },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./template/index.html",
+    }),
+  ],
   mode: "production",
 };
